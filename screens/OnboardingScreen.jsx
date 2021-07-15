eab65d0fdba4a818e76de6bfc1e3a1338b80ce40
Prince Narteh <prinarteh@gmail.com>
2021-07-15 13:42:42 +0000

    initial commit

---
+++ screens/OnboardingScreen.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+
+const OnboardingScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text>Onboarding Screen</Text>
+      <Button title="Click Here" onPress={() => navigation.navigate("Login")} />
+    </View>
+  );
+};
+
+export default OnboardingScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
